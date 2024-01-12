c2497d03fde1c060f35564c8a849761fdb81e03b
Rolf Bansbach <rolfisub@gmail.com>
2024-01-12 17:40:05 -0500

    new layout and also react router

--- frontend/web/src/App.tsx
+++ frontend/web/src/App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import LayoutHome from "./components/LayoutHome";
 
 function App() {
-  return <LayoutHome />;
+  return <LayoutHome userName={"Rolf Bansbach Benavides"} />;
 }
 
 export default App;
