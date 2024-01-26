472ff65942b2a73701951b0bc9c1d97aa3195e6c
Rolf Bansbach <rolfisub@gmail.com>
2024-01-26 05:46:22 -0500

    contact me form

--- frontend/web/src/routes/pages/ContactMe.tsx
+++ frontend/web/src/routes/pages/ContactMe.tsx
@@ -1,5 +1,90 @@
-import React from "react";
+import React, { useState } from "react";
+import { TextField, Button, Grid, Paper, Typography } from "@mui/material";
 
-export default function ContactMe() {
-  return <div>Contact Me</div>;
-}
+const ContactMeForm = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const handleChange = (event: any) => {
+    const { name, value } = event.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (event: any) => {
+    event.preventDefault();
+    // Add your logic to handle form submission (e.g., sending an email)
+    console.log("Form Data:", formData);
+    // Reset form after submission
+    setFormData({
+      name: "",
+      email: "",
+      message: "",
+    });
+  };
+
+  return (
+    <Paper sx={{ padding: 3 }}>
+      <Typography variant="h5" align="center">
+        Contact Me
+      </Typography>
+      <form style={{ wordSpacing: 3 }} onSubmit={handleSubmit}>
+        <Grid container spacing={3}>
+          <Grid item xs={12}>
+            <TextField
+              label="Name"
+              variant="outlined"
+              fullWidth
+              required
+              name="name"
+              value={formData.name}
+              onChange={handleChange}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              label="Email"
+              variant="outlined"
+              fullWidth
+              required
+              type="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              label="Message"
+              variant="outlined"
+              fullWidth
+              required
+              multiline
+              rows={4}
+              name="message"
+              value={formData.message}
+              onChange={handleChange}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <Button
+              type="submit"
+              variant="contained"
+              color="primary"
+              fullWidth
+              sx={{
+                wordSpacing: 2,
+              }}
+            >
+              Submit
+            </Button>
+          </Grid>
+        </Grid>
+      </form>
+    </Paper>
+  );
+};
+
+export default ContactMeForm;
